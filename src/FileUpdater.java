4917a56f7d31e3724b3faf4cca1290431624c674
Justin Pu <aurpine@users.noreply.github.com>
2016-06-21 15:31:44 -0400

    Update FileUpdater.java

--- src/FileUpdater.java
+++ src/FileUpdater.java
@@ -2,23 +2,27 @@ import java.nio.file.*;
 import java.util.*;
 import java.io.*;
 
-public class FileUpdater {
+public class SnippetUpdater {
   
-  public static final String USER_DIR = "test/";//specifies directory to update files in (and all sub-directories within)
+  public static final String USER_DIR = "INSERT DIRECTORY HERE";//specifies directory to update files in (and all sub-directories within)
   //public static final String FILE_FILTER = "*.{html}";//add additional file types with commas, e.g. '*.{html,css,java}'
   public static final String SIGNIFIER = "<!--MCPT-->";//signifies whether file should be updated
   public static List <String> BEGIN_HTML_CONTENTS, END_HTML_CONTENTS;
   
+  public static boolean DEBUG = false;//set to true when debugging
+  
   /* Replaces everything between the start of the file to the <main> tag with the contents of 'begin.html', and
    * replaces everything between </main> and the end of the file with the contents of 'end.html'.
-   * 
+   *
    * (Only happens if first line of file is <!--MCPT--> AND the file is not 'begin.html' or 'end.html')
    */
   
   public static void update (Path p) {
-    if (!p.getFileName ().equals ("begin.html") && !p.getFileName ().equals ("end.html")) {
+    if (!p.getFileName ().equals ("head.html") && !p.getFileName ().equals ("tail.html")) {
       try (BufferedReader in = new BufferedReader (new FileReader (p.toFile ()))) {
-        if (in.readLine ().equals (SIGNIFIER)) {//first line of file signifies whether file should be updated
+        String firstLine = in.readLine ();
+        
+        if (firstLine != null && firstLine.equals (SIGNIFIER)) {//first line of file signifies whether file should be updated
           in.close ();
           List <String> text = readAllLines (p);
           
@@ -26,19 +30,44 @@ public class FileUpdater {
           
           //Prints signifier, and contents of begin.html as well as <main> tag at the end
           out.println (SIGNIFIER);
-          out.println (BEGIN_HTML_CONTENTS);
-          out.println ("<main>");
           
-          //Prints out all normal contents of the file between <main> and </main>
-          int pos = text.indexOf ("<main>");
-          
-          for (int i = pos + 1; i < text.size () && !text.get (i).equals ("</main>"); i++) {
-            out.println (text.get (i));
+          if (DEBUG) {
+            for (String i : BEGIN_HTML_CONTENTS) {
+              System.out.println (i);
+            }
           }
           
-          //Prints </main> as well as contents of end.html
-          out.println ("</main>");
-          out.println (END_HTML_CONTENTS);
+          for (int i = 0; i < BEGIN_HTML_CONTENTS.size (); i++) {
+            out.print (BEGIN_HTML_CONTENTS.get (i));
+            
+            if (i != BEGIN_HTML_CONTENTS.size () - 1) {
+              out.println ();
+            }
+          }
+          
+          //finds position of <main> tag
+          int pos = text.size ();
+          
+          for (int i = 0; i < text.size (); i++) {
+            if (text.get (i).trim ().equals ("<main>")) {
+              pos = i;
+              break;
+            }
+          }
+          
+          //prints out all contents between <main> and </main> (including the two tags)
+          for (int i = pos; i < text.size (); i++) {
+            out.println (text.get (i));
+            
+            if (text.get (i).trim ().equals ("</main>")) {
+              break;
+            }
+          }
+          
+          for (String i : END_HTML_CONTENTS) {
+            out.println (i);
+          }
+          
           out.close ();
         }
       }
@@ -49,7 +78,7 @@ public class FileUpdater {
   }
   
   /* Gets all file paths specified by the filter in a given directory (including all sub-directories)
-   * 
+   *
    */
   
   public static List <Path> getSourcePaths (Path dir) {
@@ -64,15 +93,15 @@ public class FileUpdater {
       curr = queue.poll ();
       
       try (DirectoryStream <Path> stream = Files.newDirectoryStream (curr /*, FILE_FILTER*/)) {
-        for (Path p : stream) {          
+        for (Path p : stream) {
           if (Files.isDirectory (p) || p.getFileName ().toString ().endsWith (".html")) {
             if (!Files.isDirectory (p)) {
               paths.add (p);
               
-              if (p.getFileName ().toString ().equals ("begin.html")) {
+              if (p.getFileName ().toString ().equals ("head.html")) {
                 BEGIN_HTML_CONTENTS = readAllLines (p);
               }
-              else if (p.getFileName ().toString ().equals ("end.html")) {
+              else if (p.getFileName ().toString ().equals ("tail.html")) {
                 END_HTML_CONTENTS = readAllLines (p);
               }
             }
@@ -91,34 +120,38 @@ public class FileUpdater {
   }
   
   /* Read file from path and get all contents as a List of Strings.
-   * 
+   *
    */
   
   public static List <String> readAllLines (Path p) throws IOException {
     BufferedReader in = new BufferedReader (new FileReader (p.toFile ()));
-    String ln = in.readLine ();
     List <String> text = new ArrayList <String> ();
+    String ln;
     
-    while (ln != null) {
-      text.add (ln.trim ());
-      ln = in.readLine ();
+    while ((ln = in.readLine ()) != null) {
+      text.add (ln);
     }
     
     return text;
   }
   
   /* Starts process
-   * 
+   *
    */
   
   public static void main (String [] args) {
     List <Path> paths = getSourcePaths (Paths.get (USER_DIR));
     
-    //System.out.println (BEGIN_HTML_CONTENTS);
-    //System.out.println (END_HTML_CONTENTS);
+    if (DEBUG) {
+      System.out.println (BEGIN_HTML_CONTENTS);
+      System.out.println (END_HTML_CONTENTS);
+    }
     
     for (Path p : paths) {
-      //System.out.println (p);
+      if (DEBUG) {
+        System.out.println (p);
+      }
+      
       update (p);
     }
   }
